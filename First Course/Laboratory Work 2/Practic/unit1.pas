f8a74c5ad268b12e935e8cc1221397e0b5c6bdbb
DeepDagon <DeepDagon@github.com>
2018-09-19 20:08:59 +0500

    Laboratory Works 2

---
+++ First Course/Laboratory Work 2/Practic/unit1.pas
@@ -0,0 +1,76 @@
+(* 1. Обменять значения двух переменных, используя третью переменную *)
+(* 2. Найти значение функции для выражения 1 из таблицы *)
+
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    Button1: TButton;
+    InfoLabelA: TLabel;
+    InfoLabelB: TLabel;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    RunButton: TButton;
+    AEdit: TEdit;
+    BEdit: TEdit;
+    procedure Button1Click(Sender: TObject);
+    procedure RunButtonClick(Sender: TObject);
+
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  MainForm: TMainForm;
+  a, b, c, x: integer;
+  y :real;
+  s :string;
+
+implementation
+
+{$R *.lfm}
+
+{ TMainForm }
+
+procedure TMainForm.RunButtonClick(Sender: TObject);
+begin
+ (* Переменные принимают значения заданные в соответствующих полях ввода *)
+ a := strtoint(AEdit.Text);
+ b := strtoint(BEdit.Text);
+
+ (* Выполнение условия задачи *)
+ (* c - это буфферная переменная *)
+ c := a;
+ a := b;
+ b := c;
+
+ (* Информация о значении переменных в реальном времени *)
+ InfoLabelA.Caption := 'Переменная a равна: ' + inttostr(a);
+ InfoLabelB.Caption := 'Переменная b равна: ' + inttostr(b);
+
+end;
+
+ (* Второе задание *)
+procedure TMainForm.Button1Click(Sender: TObject);
+begin
+ x := 1;
+ y := ln(Exp(x) + Sqrt(1+Exp(2*x)));
+ Str(y : 5 : 2, s);
+ Label3.Caption := 'Выражение равняется: ' + s;
+end;
+
+end.
+
